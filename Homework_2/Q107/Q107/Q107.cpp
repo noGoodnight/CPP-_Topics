4f7144a1fdc823845d93c5eb03eb98dc96fcd6d5
SunriseToSunset <m15256072859@163.com>
2020-03-09 12:35:06 +0800

    matebook

--- Homework_2/Q107/Q107/Q107.cpp
+++ Homework_2/Q107/Q107/Q107.cpp
@@ -14,16 +14,16 @@ int main() {
 		if (c == '-') {
 			continue;
 		}
-		else if('a' <= c && c <= 'z' ){
+		else if ('a' <= c && c <= 'z') {
 			p[len++] = c - 32;
 		}
 		else {
 			p[len++] = c;
 		}
 	}
-	
+
 	cin >> numOfParts;
-	
+
 	int temp = len;
 	for (int i = 0; i < len / numOfParts; i++) {
 		for (int j = 0; j < numOfParts; j++) {
